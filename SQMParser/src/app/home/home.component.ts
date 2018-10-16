9aad02a451f14de755b69ddde6c842a2a5a952af
DannyDorito <32311622+DannyDorito@users.noreply.github.com>
2018-10-16 15:32:42 +0100

    implemented saving uploaded file to var, not local storage

--- SQMParser/src/app/home/home.component.ts
+++ SQMParser/src/app/home/home.component.ts
@@ -10,26 +10,30 @@ export class HomeComponent implements OnInit {
   selectedFile: any;
   localStorageFileName = 'text';
 
-  onFileChanged(event: any) {
-    // Based on: https://www.academind.com/learn/angular/snippets/angular-image-upload-made-easy/ Accessed 9th October 2018
+  onFileChanged(fileChangeEvent: any) {
     const fileReader = new FileReader();
-    fileReader.readAsText(event.target.files[0], 'UTF-8');
-    fileReader.onload = function(fileReaderEvent: any) {
-      localStorage.setItem('text', fileReaderEvent.target.result);
-    };
-    this.selectedFile = localStorage.getItem(this.localStorageFileName);
+    fileReader.onload = (function(f) {
+      return function(readerEvent) {
+        this.selectedFile = readerEvent.target.result;
+      };
+    })(fileChangeEvent.target.files[0]);
+    fileReader.readAsText(fileChangeEvent.target.files[0], 'UTF-8');
   }
 
   ngOnInit() {
-    this.loadFromLocalStorage();
+    // this.loadFromLocalStorage();
   }
 
   loadFromLocalStorage() {
-    if (!isNullOrUndefined(this.selectedFile)) {
+    if (this.selectedFile !== null && this.selectedFile !== '') {
       this.selectedFile = localStorage.getItem(this.localStorageFileName);
       return true;
     } else {
       return false;
     }
+  }
+
+  saveLocalStorage(file: any) {
+    localStorage.setItem(this.localStorageFileName, file)
   }
 }
