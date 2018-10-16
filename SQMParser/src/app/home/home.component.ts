34403b3f6007be9d2aa61af2a87d956c6a064b48
DannyDorito <32311622+DannyDorito@users.noreply.github.com>
2018-10-16 15:45:53 +0100

    updated references

--- SQMParser/src/app/home/home.component.ts
+++ SQMParser/src/app/home/home.component.ts
@@ -9,8 +9,10 @@ export class HomeComponent implements OnInit {
   selectedFile: any;
   localStorageFileName = 'text';
 
+  // Based on: https://www.academind.com/learn/angular/snippets/angular-image-upload-made-easy/ Accessed 9th October 2018
   onFileChanged(fileChangeEvent: any) {
     const fileReader = new FileReader();
+    // Based on: https://stackoverflow.com/a/27439524 Accessed 16th October 2018
     fileReader.onload = (function(f) {
       return function(readerEvent) {
         this.selectedFile = readerEvent.target.result;
