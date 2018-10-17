798a3b27725cfd410bcc10f641b4bb08d591b1e5
DannyDorito <32311622+DannyDorito@users.noreply.github.com>
2018-10-17 21:09:41 +0100

    basic local storage options

--- SQMParser/src/app/app.component.ts
+++ SQMParser/src/app/app.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  isOptions = false;
+
+  ngOnInit() {
+    if (!this.hasLocalStorage('sqm_hasOptions')) {
+      localStorage.setItem('sqm_hasOptions', 'true');
+    }
+  }
+
+  changeOptions() {
+    if (this.isOptions) {
+      this.isOptions = false;
+    } else {
+      this.isOptions = true;
+    }
+  }
+
+  hasLocalStorage(accessor: string) {
+    const storageItem = localStorage.getItem(accessor);
+    if (storageItem === '') {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
