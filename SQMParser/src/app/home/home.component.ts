9c18ed4b3b478518a47653a487a04600447946bc
DannyDorito <32311622+DannyDorito@users.noreply.github.com>
2018-11-07 00:04:59 +0000

    moved saving and edit functionality to view tree, added astToStrArray for saveFile

--- SQMParser/src/app/home/home.component.ts
+++ SQMParser/src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, AfterViewInit, ViewChild } from '@angular/core';
-import * as FileSaver from 'file-saver';
 import { isNullOrUndefined } from 'util';
 import { ParserService } from '../parser/parser.service';
 import { AST } from '../shared/ast';
@@ -66,31 +65,6 @@ export class HomeComponent implements AfterViewInit {
   ngAfterViewInit() {
     if (!isNullOrUndefined(this.tree)) {
       this.viewTree.tree = this.tree;
-    }
-  }
-
-  editItem( pos: number ) {
-    console.log( pos );
-  }
-
-  /**
-   * Saves given file to passed fileName, appends .sqm if it does not have it
-   * Based on:
-   * https://github.com/eligrey/FileSaver.js [Online] Accessed 19th October 2018
-   * https://github.com/eligrey/FileSaver.js/issues/308#issuecomment-286127364  [Online] Accessed 20th October 2018
-   * https://github.com/eligrey/FileSaver.js/blob/master/README.md#supported-browsers [Online] Accessed 20th October 2018
-   */
-  saveFile( fileName: string ) {
-    try {
-      const isFileSaverSupported = !!new Blob;
-      if ( !fileName.includes( '.sqm' ) ) {
-        fileName += '.sqm';
-      }
-      // FileSaver.saveAs( new Blob( this.file, {
-      //   type: 'text/plain;charset=utf-8'
-      // } ), fileName );
-    } catch ( exception ) {
-      console.log( exception );
     }
   }
 }
--- SQMParser/src/app/view-tree/view-tree.component.ts
+++ SQMParser/src/app/view-tree/view-tree.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
+import * as FileSaver from 'file-saver';
 import { AST } from '../shared/ast';
+import { isNullOrUndefined } from 'util';
 
 @Component( {
   selector: 'app-view-tree',
@@ -8,4 +10,47 @@ import { AST } from '../shared/ast';
 } )
 export class ViewTreeComponent {
   @Input() tree: AST[];
+
+    /**
+   * Saves given file to passed fileName, appends .sqm if it does not have it
+   * Based on:
+   * https://github.com/eligrey/FileSaver.js [Online] Accessed 19th October 2018
+   * https://github.com/eligrey/FileSaver.js/issues/308#issuecomment-286127364  [Online] Accessed 20th October 2018
+   * https://github.com/eligrey/FileSaver.js/blob/master/README.md#supported-browsers [Online] Accessed 20th October 2018
+   */
+  saveFile( fileName: string ) {
+    try {
+      const isFileSaverSupported = !!new Blob;
+      if ( !fileName.includes( '.sqm' ) ) {
+        fileName += '.sqm';
+      }
+      FileSaver.saveAs( new Blob( this.astToStrArray(this.tree), {
+        type: 'text/plain;charset=utf-8'
+      } ), fileName );
+    } catch ( exception ) {
+      console.log( exception );
+    }
+  }
+
+  editItem( pos: number ) {
+    console.log( pos );
+  }
+
+  /**
+   * Converts passed AST tree to string[] for saving to file
+   */
+  astToStrArray( tree: AST[] ) {
+    const stringTree: string[] = [];
+    tree.forEach(branch => {
+      let stringConcat = branch.item.value;
+      branch.children.forEach(child => {
+        if (!isNullOrUndefined(child)) {
+          stringConcat += child.item.value;
+        }
+      });
+      stringConcat += '\r\n';
+      stringTree.push(stringConcat);
+    });
+    return stringTree;
+  }
 }
