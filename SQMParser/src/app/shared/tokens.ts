bc69511c3ada3843c31425ef03d69a298b904643
DannyDorito <32311622+DannyDorito@users.noreply.github.com>
2018-11-07 20:54:40 +0000

    fixes grammar definitions

--- SQMParser/src/app/shared/tokens.ts
+++ SQMParser/src/app/shared/tokens.ts
@@ -19,8 +19,8 @@ export enum Grammar {
   STRING = Token.STRING + Token.EQUALS + Token.QUOTE + Token.STRING + Token.QUOTE + Token.SEMICOLON,
   BOOLEAN = Token.STRING + Token.EQUALS + Token.BOOLEAN + Token.SEMICOLON,
   NUMBER = Token.STRING + Token.EQUALS + Token.NUMBER + Token.SEMICOLON,
-  ARRAY = Token.STRING + Token.START_SQUARE_BRACE + Token.EQUALS + Token.START_BRACE + Token.ARRAY_CONTENTS + Token.END_BRACE + Token.SEMICOLON,
-  CLASS = Token.STRING + Token.EQUALS + Token.STRING + Token.START_BRACE + Token.CLASS_CONTENTS + Token.END_BRACE,
+  ARRAY = Token.STRING + Token.START_SQUARE_BRACE + Token.EQUALS + Token.START_BRACE,
+  CLASS = Token.STRING + Token.EQUALS + Token.STRING + Token.START_BRACE,
   END = Token.END_BRACE + Token.SEMICOLON,
   START = Token.START_BRACE
 }
