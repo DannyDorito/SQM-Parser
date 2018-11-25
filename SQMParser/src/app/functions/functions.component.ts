28bd33eb2f10c427403a361244b9632df6607a8a
DannyDorito <32311622+DannyDorito@users.noreply.github.com>
2018-11-25 19:52:56 +0000

    added removal of unused dependencies

--- SQMParser/src/app/functions/functions.component.ts
+++ SQMParser/src/app/functions/functions.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ParserSharedService } from '../parser/parsershared.service';
 import { SaverService } from '../saver/saver.service';
 import { ASTNode } from '../shared/ast';
+import { isNullOrUndefined } from 'util';
 
 @Component({
   selector: 'app-functions',
@@ -45,5 +46,36 @@ export class FunctionsComponent {
    */
   async clearSQM() {
     this.saver.clearSQM();
+  }
+
+  /**
+   * Removes addOns and addOnsAuto dependencies from mission ast
+   */
+  removeDependencies() {
+    let missionAST: ASTNode[];
+    this.parserShared.getMissionAST().subscribe(ast => {
+      missionAST = ast as ASTNode[];
+    });
+    const addOns = this.getIndex('addOns', missionAST, 0);
+    if (!isNullOrUndefined(addOns)) {
+      missionAST.splice(addOns, this.getIndex('}', missionAST, addOns));
+    }
+    const addOnsAutoIndex = this.getIndex('addOnsAuto', missionAST, 0);
+    if (!isNullOrUndefined(addOnsAutoIndex)) {
+      missionAST.splice(addOnsAutoIndex, this.getIndex('}', missionAST, addOnsAutoIndex));
+    }
+    this.parserShared.setMissionAST(missionAST);
+  }
+
+  /**
+   * Get the index of an item from the missionAST tree
+   */
+  getIndex(value: string, missionAST: ASTNode[], index: number) {
+    for (index < missionAST.length; index++;) {
+      if (missionAST[index].value === value) {
+        return index;
+      }
+    }
+    return undefined;
   }
 }
