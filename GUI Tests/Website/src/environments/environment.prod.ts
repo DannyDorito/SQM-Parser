997f7c1845655713be879aa633f6d8087850d23d
DannyDorito <32311622+DannyDorito@users.noreply.github.com>
2019-01-07 15:25:59 +0000

    removed test website

--- GUI Tests/Website/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  baseUri: 'http://localhost:4200/api/',
-  tracing: false
-};
--- GUI Tests/Website/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  baseUri: 'http://localhost:4200/api/',
-  tracing: false
-};
