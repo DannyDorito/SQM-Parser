5673e97347b318733ea8ef30de67456afa491d4e
DannyDorito <32311622+DannyDorito@users.noreply.github.com>
2019-01-29 13:54:32 +0000

    inject template data

--- SQMParser/src/app/class-template/class-template.component.ts
+++ SQMParser/src/app/class-template/class-template.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NPCTemplate, PlayerTemplate, VehicleTemplate, WeatherTemplate, IntroTemplate, OutroTemplate, SquadTemplate, ObjectTemplate } from '../shared/arma-template';
+
 
 @Component({
   selector: 'app-class-template',
   templateUrl: './class-template.component.html',
   styleUrls: ['./class-template.component.css']
 })
-export class ClassTemplateComponent implements OnInit {
+export class ClassTemplateComponent {
 
-  constructor() { }
+  constructor(public dialogueRef: MatDialogRef<ClassTemplateComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: NPCTemplate | PlayerTemplate | VehicleTemplate | WeatherTemplate | IntroTemplate | OutroTemplate | SquadTemplate | ObjectTemplate ) {}
 
-  ngOnInit() {
+  onNoClick() {
+    this.dialogueRef.close();
   }
 
 }
