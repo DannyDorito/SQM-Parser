46dd0dfeef6ceecbddef44577649f5eff6cfccfe
DannyDorito <32311622+DannyDorito@users.noreply.github.com>
2019-01-29 15:43:46 +0000

    Update not-found.component.ts

--- SQMParser/src/app/not-found/not-found.component.ts
+++ SQMParser/src/app/not-found/not-found.component.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-not-found',
   template: `
   <div>
-    <h1>Page Not Found</h1>
-    <button [routerLink]="['']">Home</button>
+    <h1 class="unselectable">Page Not Found</h1>
+    <button [routerLink]="['']" tab-index="0">Home</button>
   </div>
   `,
   styles: ['div { display: flex;flex-direction: column;height: 100%; justify-content: center;} button { margin-top: 10px; margin-left: auto;} ']
