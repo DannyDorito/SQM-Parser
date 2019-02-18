c7a51aa2410c14b2a7cfc2a1eedce9c9bae33cb5
DannyDorito <32311622+DannyDorito@users.noreply.github.com>
2019-02-18 14:38:01 +0000

    removed unused mat & cdk modules

--- SQMParser/src/app/app.module.ts
+++ SQMParser/src/app/app.module.ts
@@ -1,11 +1,9 @@
-import { ScrollingModule } from '@angular/cdk/scrolling';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatTreeModule } from '@angular/material/tree';
 import { BrowserModule } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
@@ -30,14 +28,12 @@ import { NotFoundComponent } from './not-found/not-found.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ScrollingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     NoopAnimationsModule,
     MatDialogModule,
     MatMenuModule,
     MatTreeModule,
     MatButtonModule,
-    MatProgressBarModule,
     MatIconModule
   ],
   providers: [AppComponent],
