ac016137f73b94d9c46ed0d37b55f3daba47cee7
DannyDorito <32311622+DannyDorito@users.noreply.github.com>
2019-03-13 11:18:51 +0000

    removed functions from router

--- SQMParser/src/app/app-routing.module.ts
+++ SQMParser/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FunctionsComponent } from './functions/functions.component';
 import { MainComponent } from './main/main.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-  { path: 'functions', component: FunctionsComponent },
   { path: '', component: MainComponent, pathMatch: 'full' },
   { path: '**', component: NotFoundComponent }
 ];
